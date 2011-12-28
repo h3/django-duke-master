b41e448b29617cadff4c60256b192c6f61f33cc3
Maxime Haineault <haineault@gmail.com>
2011-12-28 10:28:51 -0500

    list projects works

--- dukemaster/commands/__init__.py
+++ dukemaster/commands/__init__.py
@@ -1,14 +1,24 @@
 from abc import abstractmethod
 
 class BaseCommand(object):
+
+    def __init__(self, data):
+        """
+        {'flags': {}, 'args': [], 'command': 'list', 'data': {'message_id': 'd1c8d2e5e93444e6b530145b982b7ea3', '__dukeclient__': {'version': '0.0.1-alpha'}}}
+        """
+        self.args = data['args']
+        self.kwargs = data['flags']
+        self.command = data['command']
+        self._meta = data['data']
+
     @abstractmethod
     def call(self):
-        print "CALLING COMMAND"
+        pass
     
 
 def call_command(data):
     cmd = data['command']
     class_name = '%sCommand' % (cmd[0].capitalize() + cmd[1:])
     module  = __import__('dukemaster.commands.%s' % cmd, {}, {}, class_name)
-    command = getattr(module, class_name)()
-    return command.call(data)
+    command = getattr(module, class_name)(data)
+    return command.call()
--- dukemaster/commands/list.py
+++ dukemaster/commands/list.py
@@ -1,11 +1,30 @@
 from dukemaster.commands import BaseCommand
 from dukemaster.servers.models import Server
+from dukemaster.projects.models import Project
+
 
 class ListCommand(BaseCommand):
+    """
+    Returns a list of either projects or servers
+    """
 
-    def call(self, data):
+    def _list_servers(self):
         out = ["\n"]
         for server in Server.objects.all():
             out.append(" - %s: %s\n" % (server.name, server.hostname))
         return "".join(out)
+
+    def _list_projects(self):
+        out = ["\n"]
+        for project in Project.objects.all():
+            out.append(" - %s\n" % project.name)
+        return "".join(out)
+
+    def call(self):
+        if self.args[0] == 'projects':
+            return self._list_projects()
+        elif self.args[0] == 'servers':
+            return self._list_servers()
+        else:
+            return "Error: unknown argument: %s" % self.args[0]
 
