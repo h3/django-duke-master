db0b10fbf4ba6f9a9a5d04ae1e4afd63697c0657
Maxime Haineault <haineault@gmail.com>
2011-12-28 01:44:28 -0500

    Changed models

--- dukemaster/projects/models.py
+++ dukemaster/projects/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from dukemaster.servers.models import Server
 
+# TODO: dynamic (with plugin architecture)
+REPOSITORY_TYPES = (
+    ('git', 'Git'),
+    ('svn', 'Subversion'),
+)
+
+
 class Project(models.Model):
     name = models.CharField(u"Name", max_length=250)
 
@@ -10,7 +17,10 @@ class Project(models.Model):
 
 class ProjectSource(models.Model):
     url = models.CharField(u"Source URL", max_length=250)
+    protocol = models.CharField(u"Type", max_length=5, choices=REPOSITORY_TYPES, default="git")
     project = models.OneToOneField(Project)
+    password = models.CharField(u"Password", max_length=250, blank=True, null=True)
+    ssh_key = models.CharField(u"SSH key", max_length=250, blank=True, null=True)
 
     def __unicode__(self):
         return u"%s" % self.url
